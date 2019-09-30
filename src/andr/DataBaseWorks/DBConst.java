a10e7879e8ee785551988232c528ddc7fe8393ae
vidroid16 <shalyaandrey16@gmail.com>
2019-09-30 03:41:21 +0300

    Final End

---
+++ src/andr/DataBaseWorks/DBConst.java
@@ -0,0 +1,29 @@
+package andr.DataBaseWorks;
+
+public class DBConst {
+    public static final String PASSPORT_TABLE = "passports";
+    public static final String PHOTO_TABLE = "photos";
+    public static final String BIRTHDATE_TABLE = "bdates";
+    public static final String USER_TABLE = "users";
+
+    public static final String NICKNAME = "nick";
+    public static final String PASSWORD = "password";
+    public static final String EMAIL = "email";
+
+    public static final String USER_PASS = "user_login";
+    public static final String NAME_PASS = "name";
+    public static final String SERNAME_PASS = "sername";
+    public static final String PATR_PASS = "patronymic";
+    public static final String ID_PASS = "ID";
+    public static final String TIME_PASS = "time";
+
+
+    public static final String IS_COLORED_PHOTO = "isColored";
+    public static final String HEIGHT_PHOTO = "height";
+    public static final String WIDTH_PHOTO = "width";
+    public static final String LINK_PHOTO = "link";
+
+    public static final String DAY_BIRTH = "day";
+    public static final String MON_BIRTH = "month";
+    public static final String YEAR_BIRTH = "year";
+}
--- src/andr/Main.java
+++ src/andr/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static Hashtable<Integer, Passport> hashtable = new Hashtable<>();
     public static ByteBuffer buffer = ByteBuffer.allocate(800000);
     public static ArrayList<Integer> userIds = new ArrayList<>();
-
+    public static final String mailRegexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     public static void main(String[] args) throws IOException {
         FileController fileController = new FileController("file.xml");
         Scanner scanner = new Scanner(System.in);
@@ -44,10 +44,19 @@ public class Main {
         while(true){
             try{
                 MyPackage sPack = null;
-
                 if(user_id == 0){
-                    //log();
-                    sPack = new MyPackage(100, user_id, null);
+                    System.out.println("Введите login - для входа \nВведите register - для регистрации ");
+                    while(sPack == null){
+                        String enter = scanner.nextLine();
+                        if(enter.equals("login")){
+                            sPack = Main.log();
+                        }else if (enter.equals("register")){
+                            sPack = Main.reg();
+                        }else{
+                            System.out.println("Введите login - для входа\n Введите register - для регистрации");
+                        }
+                    }
+                    //sPack = new MyPackage(100, user_id, null);
                 }else{
                     String line = scanner.nextLine();
                     //System.out.println("ВЫ ВВЕЛИ КОММАНДУ");
@@ -108,28 +117,46 @@ public class Main {
 
 
     }
-    public MyPackage log(){
+    private static MyPackage log(){
         Scanner scanner = new Scanner(System.in);
-        String login;
-        String password;
-        while(true) {
-            login = scanner.nextLine();
-            password = scanner.nextLine();
-            if (login.matches("[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}") && password.matches("/[A-Za-z0-9]/")) break;
+        String login = "";
+        String password = "";
+        System.out.print("Введеите логин: ");
+        login = scanner.nextLine();
+        System.out.print("Введите пароль: ");
+        password = scanner.nextLine();
+
+        if (login.matches("^[a-z\\d][a-z\\d]*[_-]?[a-z\\d]*[a-z\\d]$") && password.matches("^[a-z\\d][a-z\\d]*[_-]?[a-z\\d]*[a-z\\d]$")
+                            && login.length()>3 && login.length()<13 && password.length()>6 && password.length()<13){
+            String data = login.concat(" " + password);
+            return new MyPackage(200, 0, data.getBytes());
+        }else{
             System.out.println("Некорректно введены данные для входа");
+            return null;
         }
-        String data = login.concat(" " + password);
-        return new MyPackage(200, 0, data.getBytes());
     }
-    public MyPackage reg(){
+    private static MyPackage reg(){
         Scanner scanner = new Scanner(System.in);
-        String login;
-        while(true){
-            login = scanner.nextLine();
-            if(login.matches("[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}")) break;
-            System.out.println("Некорректно введена почта");
+        String login = "";
+        String password = "";
+        String mail = "";
+        System.out.print("Введеите почту: ");
+        mail = scanner.nextLine();
+        System.out.print("Введеите логин: ");
+        login = scanner.nextLine();
+        System.out.print("Введите пароль: ");
+        password = scanner.nextLine();
+
+        if(mail.matches(Main.mailRegexp) && login.matches("^[a-z\\d][a-z\\d]*[_-]?[a-z\\d]*[a-z\\d]$")
+                        && password.matches("^[a-z\\d][a-z\\d]*[_-]?[a-z\\d]*[a-z\\d]$")
+                        && login.length()>3 && login.length()<13 && password.length()>6 && password.length()<13){
+            String rez = mail.concat(" "+ login + " " + password);
+            return new MyPackage(201, 0, rez.getBytes());
+        }else{
+            System.out.println("Некорректно введены данные для регистрации.\n " +
+                    "Проверьте, что ваши логин и пароль состоят только из латинсиких букв и цифр, и имеют длину > 3 и <13 символов");
+            return null;
         }
-        return new MyPackage(201, 0, login.getBytes());
     }
     public static class ProgramPauser{
         public static void pause(String message){
